8114b69d5783f2afd65cc56ebc2075216aa989bf
Linhyss <1290113987@qq.com>
2019-05-05 10:24:15 +0800

    完善2

---
+++ app/src/main/java/android/linhyss/myweather/util/HttpUtil.java
@@ -0,0 +1,20 @@
+package android.linhyss.myweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Administrator on 2019/5/4.
+ */
+
+public class HttpUtil {
+    public static void  sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient okHttpClient=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        okHttpClient.newCall(request).enqueue(callback);
+
+    }
+
+
+
+}
